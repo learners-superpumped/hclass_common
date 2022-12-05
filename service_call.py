7b52ad0f27e3da0347c4fa0955885109ea93c175
todhm <todhm@nate.com>
2022-12-05 17:28:59 +0900

    add requests message test

--- service_call.py
+++ service_call.py
@@ -21,5 +21,5 @@ def get_message_server_template(template_type: str) -> Dict:
 def send_message(message_dict: Dict, is_test: bool = True) -> Dict:
     if is_test:
         return message_dict
-    response = requests.get(f"{MESSAGE_SERVER_ENDPOINT}/messages", json=message_dict, headers=MESSAGE_SERVER_HEADERS)
+    response = requests.post(f"{MESSAGE_SERVER_ENDPOINT}/messages", json=message_dict, headers=MESSAGE_SERVER_HEADERS)
     return response.json()
